aa7af1e1e6085411be49f5e361c7a2e1efab7c1a
shafeeq <eshafeeqe@gmail.com>
2019-10-05 15:35:56 +1000

    Enemy movement and rubbish creation updated

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -26,21 +26,23 @@ public class EnemyMovement : MonoBehaviour
     float maxZ = 10.0f;
 
     GameObject[] rubishArr;
-    const int MAX_RUBISHS = 100;
+    const int MAX_RUBISHS = 500;
     int rubishIndex = 0;
     float rubishSize = 0.3f;
 
-    int rb_step = 5;
+    int rb_step = 10;
     int count = 0;
 
     float randR = 5f;
     float randL = 0f;
+    float gravity = -0.07f;
+    public float thrust =100f;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.gravity = new Vector3(0, gravity, 0);
         centerPos = new Vector2(0.0f, 0.0f);
 
         x = transform.position.x;
@@ -65,13 +67,14 @@ public class EnemyMovement : MonoBehaviour
         randY = 0.8f * r * Mathf.PerlinNoise(dx, dz);
         transform.position = Vector3.Lerp(transform.position , new Vector3(dx, randY, dz), Time.deltaTime);
 
-        if (rubishIndex < MAX_RUBISHS && count% rb_step == 0)
+        if (rubishIndex < MAX_RUBISHS && count % rb_step == 0)
         {
 
             RubhishGenerate(new Vector3(transform.position.x, transform.position.y, transform.position.z));
+            
         }
+        count++;
 
-        
     }
 
     Vector2 randomPosition()
@@ -83,21 +86,36 @@ public class EnemyMovement : MonoBehaviour
         return res;
     }
 
+    Vector3 randomVector()
+    {
+        Vector3 res;
+        res.x = Random.Range(randL, randR)>0.5 ? 1 : 0;
+        res.y = Random.Range(randL, randR)>0.5 ? 1 : 0;
+        res.z = Random.Range(randL, randR)>0.5 ? 1 : 0;
+
+        return res;
+    }
+
     void RubhishGenerate(Vector3 position)
     {
         Vector2 randomPos = randomPosition(); 
         rubishArr[rubishIndex] = GameObject.CreatePrimitive(PrimitiveType.Cube);
          
         
-        rubishArr[rubishIndex].transform.position = new Vector3(position.x + randomPos.x, 2.0f, position.z + randomPos.y);
+        rubishArr[rubishIndex].transform.position = new Vector3(position.x + randomPos.x, position.y, position.z + randomPos.y);
         rubishArr[rubishIndex].transform.localScale = new Vector3(rubishSize, rubishSize, rubishSize);
       
         Rigidbody rb = rubishArr[rubishIndex].AddComponent<Rigidbody>() as Rigidbody;
+
+        Vector3 randVec = randomVector();
+        Vector3 direction = new Vector3(transform.forward.x*randVec.x, transform.forward.y* randVec.y, transform.forward.x* randVec.z);
+
+        rb.AddForce(direction * thrust);
+
+        rb.mass = rubishSize;
+        rb.useGravity = true;
         rubishIndex++;
 
-        rb.mass = rubishSize * 5.0f;
-        rb.useGravity = true;
-        
     }
 
     IEnumerator Stall()
---
+++ Assets/Scripts/ParticleMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * 10.0f;
+        if (Input.GetButton("Fire1"))
+            transform.position += transform.forward * Time.deltaTime * 40.0f;
+
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if (terrainHeightWhereWeAre > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x,
+            terrainHeightWhereWeAre,
+            transform.position.z);
+        }
+    }
+}
+
+
+                    
